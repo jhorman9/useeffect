2603d83fe5ccd0bd176d8f749b167da45b353c6e
jhorman9 <jhosmita9@gmail.com>
2022-10-25 18:13:56 -0500

    actualizar

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,9 @@ function App() {
     let getColorRandom = colors[colorLength];
     document.body.style = `background:${getColorRandom}`;
   },[isVisible]) //el [] es conocido como el arreglo de dependencia
-  //en las dependencia se ponen props o useStates(estados)
+  //en las dependencia se ponen props o useStates(estados) esto es usado para
+  //que unicamente cuando le de click a eso funcione, puedo pasar mas de uno en el arreglo
+  //de dependencia
 
   // const colors = ["#081c15","#ffb703", "#22223b", "#023e8a", "#003049", "#03071e", "#370617", "#6a040f", "#9d0208", "#dc2f02", "#e85d04", "#f48c06", "#9a031e", "#ffc300"];
   //   let colorLength = Math.floor(Math.random() * colors.length);
@@ -57,7 +59,7 @@ function App() {
   }, [count])
   return (
     <div className="App">
-      <h2 style={{color:"white", textAlign:"center"}}>{title.data.title}</h2>
+      {/* <h2 style={{color:"white", textAlign:"center"}}>{title.data.title}</h2> */}
       <div className='contain'>
         <div className="counter">
           <div className='number'>{count}</div>
